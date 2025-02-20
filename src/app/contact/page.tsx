6e648d765c73a20d2d746fa4116a3b7bdd4421c8
dokkiitech <info@dokkiitech.com>
2025-02-21 07:14:49 +0900

    update contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
-import { Twitter, Instagram } from "lucide-react"
-import Link from "next/link"
-import Header from "../../components/Header"
+import { X as Twitter, Instagram, Github } from "lucide-react";
+import Link from "next/link";
+import Header from "../../components/Header";
 
 export default function Contact() {
     return (
@@ -10,16 +10,18 @@ export default function Contact() {
                 <div className="text-center">
                     <h1 className="text-3xl font-jp mb-8">Contact Me</h1>
                     <div className="flex justify-center space-x-8">
-                        <Link href="https://x.com/dokkiitech" target="_blank" rel="noopener noreferrer">
+                        <Link href="https://twitter.com/dokkiitech" target="_blank" rel="noopener noreferrer">
                             <Twitter className="w-8 h-8 hover:text-gray-300" />
                         </Link>
                         <Link href="https://instagram.com/dokkiitech" target="_blank" rel="noopener noreferrer">
                             <Instagram className="w-8 h-8 hover:text-gray-300" />
                         </Link>
+                        <Link href="https://github.com/dokkiitech" target="_blank" rel="noopener noreferrer">
+                            <Github className="w-8 h-8 hover:text-gray-300" />
+                        </Link>
                     </div>
                 </div>
             </main>
         </div>
-    )
+    );
 }
-
